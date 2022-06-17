2698bb8964081c518dd75b3cc51de7a4757e6d68
Jobsidney <jobsidney67@gmail.com>
2022-06-17 15:28:03 +0300

    commit tree e6aaabfd83ae423a5e3bffc751c919ee01a70628

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Add your code here
+
+
+const btn=document.querySelector('#btn')
+
+let name = "Steve"
+let email = "steve@steve.com"
+btn.addEventListener('click',submitData(name,email))
+
+function submitData(fname, emailAddress) {
+const person={
+    name:fname,
+    email:emailAddress
+    // id: 659
+}
+    const configureObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify( person )
+    }
+    
+    return fetch('http://localhost:3000/users', configureObject )
+      .then( response => {
+        return response.json()
+      } )
+      .then( data => {
+        document.body.innerHTML = data[ "id" ]
+      } )
+      .catch( err => {
+        document.body.innerHTML = err.message
+      } )
+  }
+
+
+
+
+
+
+
+
